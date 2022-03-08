53e0d784727e00ee39e49429d169109c6287db09
frankmweti <100199571+frankmweti@users.noreply.github.com>
2022-03-08 18:33:53 +0300

    Add files via upload

---
+++ frank.c
@@ -0,0 +1,21 @@
+/*
+project: county library management system.
+Author:frankline mweti
+Date: Oct,2021
+Compiler: GNU GCC
+License: MIT
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCounty Library\n");
+    printf("Welcome Mr. frank.\n");
+    printf("What would you like to do?\n");
+    printf("1. Add new user\n");
+    printf("2. Add Book.\n");
+    printf("Selected actions: ");
+    return 0;
+}
